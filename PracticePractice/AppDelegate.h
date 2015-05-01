0ee9c2ed82244e2e88009d822702be85264716d8
Justin Huntington <corusco@gmail.com>
2015-05-01 15:09:33 -0600

    Initial Commit

---
+++ PracticePractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticePractice
+//
+//  Created by Justin Huntington on 5/1/15.
+//  Copyright (c) 2015 Justin Huntington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PracticePractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticePractice
+//
+//  Created by Justin Huntington on 5/1/15.
+//  Copyright (c) 2015 Justin Huntington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
